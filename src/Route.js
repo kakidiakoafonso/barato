dae1e5e00fb082e560579abe389392048b83c9c6
Kakidiako Afonso <kakidiakoafonso@gmail.com>
2021-04-10 14:47:44 +0100

    Segundo commit

--- src/Route.js
+++ src/Route.js
@@ -1,37 +1,58 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { Icon } from 'native-base'
-import React from 'react'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+//Screen Imports
+import Categorias from './View/Categorias/Categorias'
 import Detail from './View/Detail/Detail'
 import Sign from './View/Sign/Sign'
 import SignUpTelefone from './View/SignUpTelefone/SignUpTelefone'
 import Splash from './View/Splash'
 import Verify from './View/Verify/Verify'
+import Home from './View/Home/Home';
 
 //Cores 
 const corPrimaria = '#2CBF88'
 const corSecudaria = '#313131'
 const corTerciaria = '#efefef'
 
-
 const Stack = createStackNavigator()
-export default function Route() 
-{
-    return (
-        <NavigationContainer>
-            <Stack.Navigator >
-                <Stack.Screen name='splash' component={Splash} options={{headerShown:false}}/>
-                <Stack.Screen name='sign' component={Sign} options={{headerShown:false}}/>
-                <Stack.Screen name='detail' component={Detail} options={{headerShown:false}}/>
-                <Stack.Screen name='verify' component={Verify} options={{headerShown:false}}/>
-                <Stack.Screen name='signuptelefone' component={SignUpTelefone} 
-                options={{headerShown:true,title:'', 
-                headerLeft:(props)=>(<Icon name='left' type='AntDesign' 
-                    onPress={()=> console.log(props)}
-                style={{color:corTerciaria}}/>),
-                headerStyle:{backgroundColor:corPrimaria}
-                }}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <TabRoute/>
+    </NavigationContainer>
+  );
 }
+
+
+const Tab = createBottomTabNavigator();
+const TabRoute = () =>{
+    return (
+    <Tab.Navigator>
+        <Tab.Screen name="categorias" component={Categorias} />
+        <Tab.Screen name="home" component={Home} />
+        <Tab.Screen name='splash' component={Splash} />
+        <Tab.Screen name='sign' component={Sign}  />
+        <Tab.Screen  name='detail' component={Detail} />
+        <Tab.Screen name='verify' component={Verify} />
+        <Tab.Screen name='signuptelefone' component={SignUpTelefone} />
+    </Tab.Navigator>
+)}
+
+
+
+const StackRoute = () =>{
+    return (
+        <Drawer.Navigator >
+        <Drawer.Screen name="categorias" component={TabRoute} />
+        <Drawer.Screen name='splash' component={Splash} />
+        <Drawer.Screen name='sign' component={Sign}  />
+        <Drawer.Screen  name='detail' component={Detail} />
+        <Drawer.Screen name='verify' component={Verify} />
+        <Drawer.Screen name='signuptelefone' component={SignUpTelefone} />
+        </Drawer.Navigator>
+)}
